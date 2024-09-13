e94371a2c297670dd6a0f661d73e56b1d7c4a6b4
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2024-09-13 16:30:18 -0300

    feat(main): Novo desafio

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
 # Golang-Clean-Arch
 Desafio Pós Goexpert - turma 4
+
+## Como utilizar:
+
+executar o comando docker compose up -d
+
+execucar o comando make migrate
+
+Acessar a pasta cmd e executar o comando: go run main.go wire_gen.go.
+
+# Ele deve mostrar que os serviços disponíveis:
+
+Starting web server on port :8000
+Starting gRPC server on port 50051
+Starting GraphQL server on port 8080
+
+
+## Testar a API:
+
+Acessar a pasta API:
+
+Fazer a requisição para criar.
+
+HTTP/1.1 200 OK
+Date: Fri, 13 Sep 2024 18:51:18 GMT
+Content-Length: 53
+Content-Type: text/plain; charset=utf-8
+
+{
+  "id": "1",
+  "price": 100,
+  "tax": 2.5,
+  "final_price": 102.5
+}
+
+Fazer a requisição para listar
+
+HTTP/1.1 200 OK
+Date: Fri, 13 Sep 2024 18:52:18 GMT
+Content-Length: 55
+Content-Type: text/plain; charset=utf-8
+
+[
+  {
+    "id": "1",
+    "price": 100,
+    "tax": 2.5,
+    "final_price": 102.5
+  }
+]
+
+## Testar o gRPC :
+
+Acessar a raiz do projeto e executar o seguinte comando: 
+
+Golang-Clean-Arch$ evans --proto internal/infra/grpc/protofiles/order.proto --host localhost --port 50051
+
+call CreateOrder
+id (TYPE_STRING) => 2
+price (TYPE_FLOAT) => 10.0
+tax (TYPE_FLOAT) => 0.2
+{
+  "finalPrice": 10.2,
+  "id": "2",
+  "price": 10,
+  "tax": 0.2
+}
+
+
+call ListOrders
+{
+  "orders": [
+    {
+      "finalPrice": 102.5,
+      "id": "1",
+      "price": 100,
+      "tax": 2.5
+    },
+    {
+      "finalPrice": 10.2,
+      "id": "2",
+      "price": 10,
+      "tax": 0.2
+    }
+  ]
+}
+
+
+## Testar o GraphQL :
+
+Acessar a porta: http://localhost:8080/
+
+mutation createOrder {
+  createOrder(input: {id:"3", Price: 10.5, Tax: 0.5}) {
+    id
+    Price
+    Tax
+  }
+}
+
+query queryOrders {
+  listOrders {
+    id
+    Price
+    Tax
+    FinalPrice
+  }
+}
+
+{
+  "data": {
+    "listOrders": [
+      {
+        "id": "1",
+        "Price": 100,
+        "Tax": 2.5,
+        "FinalPrice": 102.5
+      },
+      {
+        "id": "2",
+        "Price": 10,
+        "Tax": 0.2,
+        "FinalPrice": 10.2
+      },
+      {
+        "id": "3",
+        "Price": 10.5,
+        "Tax": 0.5,
+        "FinalPrice": 11
+      }
+    ]
+  }
+}
---
+++ cmd/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github/felipecardosodeoliveira/golang-clean-arch/configs"
+	"github/felipecardosodeoliveira/golang-clean-arch/internal/event/handler"
+	"github/felipecardosodeoliveira/golang-clean-arch/internal/infra/graph"
+	"github/felipecardosodeoliveira/golang-clean-arch/internal/infra/grpc/pb"
+	"github/felipecardosodeoliveira/golang-clean-arch/internal/infra/grpc/service"
+	"github/felipecardosodeoliveira/golang-clean-arch/internal/infra/web/webserver"
+	"github/felipecardosodeoliveira/golang-clean-arch/pkg/events"
+
+	graphql_handler "github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/streadway/amqp"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	// mysql
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		configs.DBUser,
+		configs.DBPassword,
+		configs.DBHost,
+		configs.DBPort,
+		configs.DBName))
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// ----- RABBITMQ
+	rabbitMQChannel := getRabbitMQChannel()
+	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("OrderCreated", &handler.OrderCreatedHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+	createOrderUseCase := NewCreateOrderUseCase(db, eventDispatcher)
+	listOrderUsecase := NewListOrderUseCase(db, eventDispatcher)
+
+	// ----- WEBSERVER
+	webserver := webserver.NewWebServer(configs.WebServerPort)
+	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
+	webserver.AddHandler("POST /order", webOrderHandler.Create)
+	webserver.AddHandler("GET /order", webOrderHandler.List)
+	fmt.Println("Starting web server on port", configs.WebServerPort)
+	go webserver.Start()
+
+	// ----- GRPC_SERVER
+	grpcServer := grpc.NewServer()
+	createOrderService := service.NewOrderService(*createOrderUseCase, *listOrderUsecase)
+	pb.RegisterOrderServiceServer(grpcServer, createOrderService)
+	reflection.Register(grpcServer)
+
+	fmt.Println("Starting gRPC server on port", configs.GRPCServerPort)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", configs.GRPCServerPort))
+	if err != nil {
+		panic(err)
+	}
+	go grpcServer.Serve(lis)
+
+	// ----- GRAPHQL_SERVER
+	srv := graphql_handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CreateOrderUseCase: *createOrderUseCase,
+		ListOrderUseCase:   *listOrderUsecase,
+	}}))
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	fmt.Println("Starting GraphQL server on port", configs.GraphQLServerPort)
+	http.ListenAndServe(":"+configs.GraphQLServerPort, nil)
+}
+
+func getRabbitMQChannel() *amqp.Channel {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	return ch
+}
